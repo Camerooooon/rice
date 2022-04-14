d212d4c93c2addfec72856861eae88a5ab72b346
Camerooooon <cjpuffnstuff@gmail.com>
2022-04-14 11:04:10 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Rice
+---
+This project contains all my dot files for my laptop. It does not however contain any actually well written, well maintained, or actually good config files. This rice was made to work on a Dell XPS 9360 with a HiDPI screen. It will break if you try to use it with a 1080p screen. See my Script repository for the script I use for xRandr.
+![image1](./2022-03-20_19-22.png)
+![image1](./2022-03-20_19-23.png)
---
+++ update.sh
@@ -0,0 +1,19 @@
+echo "This script takes config files from your system and moves them here."
+read -r -p "This will overrite uncommited changes in this repository. Are you sure? [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+	    echo "Ok :)"
+        ;;
+    *)
+	    echo "Bye then..."
+	    exit 0
+        ;;
+esac
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cp ~/.config/polybar/config $SCRIPT_DIR/polybar -v
+cp ~/.config/picom.conf $SCRIPT_DIR/picom -v 
+cp ~/.config/nvim/init.vim $SCRIPT_DIR/nvim -v 
+
+echo "Done updating"
+
